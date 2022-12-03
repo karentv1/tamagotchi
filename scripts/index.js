29c34c5a4c9f9fe28c0138d884ce673ff231ee2f
Karen Vu <karentv1@uci.edu>
2022-12-03 17:17:58 -0500

    testing sprite animations

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,11 @@ canvas.width = 1024
 canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-
+let img = new Image();
+img.src = 'img/tamagotchi_ss.png';
+img.onload = function() {
+  init();
+};
 
 function changeColor() {
   const custom_color = document.querySelector('#colorpicker').value
@@ -23,4 +27,40 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-animate()
+const scale = 2;
+const width = 35;
+const height = 40;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  c.drawImage(img,
+                frameX * width, frameY * height, width, height,
+                canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+const cycleLoop = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step() {
+  frameCount++;
+  if (frameCount < 30) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = 'pink'
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+}
+
+function init() {
+  window.requestAnimationFrame(step);
+}
