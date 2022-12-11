3d2ad0f08d377155b75842271c897ad6b18e53fc
Alex van der Meulen <amvandermeulen0234@eagle.fgcu.edu>
2022-12-11 01:13:07 -0500

    added animations & more sprites

--- scripts/game.js
+++ scripts/game.js
@@ -20,9 +20,9 @@ export class GameObject {
   
   checkCollision(px, py, rx, ry, rw, rh){
     if (px >= rx &&         // right of the left edge AND
-    px <= rx + rw &&    // left of the right edge AND
-    py >= ry &&         // below the top AND
-    py <= ry + rh) {    // above the bottom
+    px <= rx + rw &&        // left of the right edge AND
+    py >= ry &&             // below the top AND
+    py <= ry + rh) {        // above the bottom
         return true;
     }
   return false;
@@ -34,33 +34,37 @@ export class TamaButton extends GameObject {
     super (x, y, height, width, vx, vy, color='white', canvas)
     this.clickEvent = clickEvent;
   }
+
+
 }
 
 export class Sprite extends GameObject {
-  constructor(x, y, height, width, vx, vy, color='white', canvas, img, offset, scale, frames, animationFrames){
+  constructor(x, y, height, width, vx, vy, color='white', canvas, img, offsetY, offsetX, scale, frames, animationFrames, animations){
     super(x, y, height, width, vx, vy, color='white', canvas)
     this.img = new Image() // image object
     this.img.src = img // image source
-    this.offset = offset
+    this.offsetY = offsetY
+    this.offsetX = offsetX
     //this.animations = animations // represents each state of the sprite (standing, sitting, etc.)
     this.scale = scale
     this.frames = frames
-    this.animationFrames = animationFrames //list of cycle loops
+    this.animationFrames = animationFrames // list of cycle loops
     this.currentFrameX = 0
     this.currentFrameY = 0; // this moves us down the y-axis for sprite sheet
     this.frameCount = 0;
+    this.animations = animations
   }
 
 
   render(){
     this.canvas.drawImage(
       this.img,
-      this.animationFrames[this.currentFrameX] * this.width, 
-      (this.currentFrameY * this.height)+this.offset,  
+      (this.animationFrames[this.currentFrameX] * this.width) + this.offsetX, 
+      (this.currentFrameY * this.height)+ this.offsetY,  
       this.width, 
       this.height,
-      this.x, 
-      this.y, 
+      this.x - (this.scale * this.width/2), 
+      this.y - (this.scale * this.height/2), 
       this.scale * this.width, 
       this.scale * this.height);
 
@@ -68,10 +72,18 @@ export class Sprite extends GameObject {
       if (this.frameCount >= this.frames) {
         this.currentFrameX++;
         this.frameCount = 0;
-        if (this.currentFrameX >= this.animationFrames.length) {
+        if (this.currentFrameX >= this.animationFrames.length) {  
           this.currentFrameX = 0;
         }
       }
 
     } //offset = 290
+
+    changeAnimation(key){
+      console.log(this.animations[key].offset)
+      this.animationFrames = this.animations[key].frames;
+      this.width = this.animations[key].width;
+      this.offsetX = this.animations[key].offset;
+      this.render();
+    }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -7,30 +7,42 @@ const canvasWidth = 1024
 const canvasHeight = 576
 
 // Define Buttons
-var playButton = new GameEngine.TamaButton(100, canvas.height-100, 25, 100, 0, 0, 'white', c, function(){
-  this.color ='pink'
-})
-var feedButton = new GameEngine.GameObject(250, canvas.height-100, 25, 100, 0, 0, 'white', c)
-var statusButton = new GameEngine.GameObject(500, canvas.height-100, 25, 100, 0, 0, 'white', c)
-var mateButton = new GameEngine.GameObject(750, canvas.height-100, 25, 100, 0, 0, 'white', c)
-let buttons = [
-  playButton,
-  feedButton,
-  statusButton,
-  mateButton
-]
+var a =  ( ) => {
+  console.log('you clicked a playbutton')
+}
+var b = () => {
+  console.log("you clicked feed")
+}
 
+
+
+var animations = {
+  idle: {
+    frames: [0,1],
+    width: 31,
+    offset:0
+  },
+  sleep:{
+    frames: [0,1,2],
+    width:30,
+    offset:447
+  }
+}
+
+console.log(animations['sleep'].frames)
+console.log(animations['sleep'].width)
 //Report the mouse position on click
 canvas.addEventListener("click", function (evt) {
   var mousePos = getMousePos(canvas, evt);
-  // alert(mousePos.x + ',' + mousePos.y);
   buttons.forEach((button => {
     if (button.checkCollision(mousePos.x, mousePos.y,
       button.x, button.y, 
       button.width, button.height)){
-        button.color = "blue";
+        button.clickEvent();      
       }}))
 }, false);
+
+
 
 //Get Mouse Position
 function getMousePos(canvas, evt) {
@@ -46,26 +58,72 @@ let spritesToRender = []
 const cycleLoop = [0,1];
 var myTamaIdle = new GameEngine.Sprite(canvasWidth/2, canvasHeight/2, 
   29, 31, 0, 0, 'white', c, "img/tamagotchi.png", 
-  290, 7, 20, cycleLoop)
+  290, 0, 7, 20, cycleLoop, animations)
+var myTamaIdle2 = new GameEngine.Sprite(canvasWidth/3, canvasHeight/3, 
+  29, 31, 0, 0, 'white', c, "img/tamagotchi.png", 
+  290, 0, 7, 100, [0,1,2,3,2,0,1], animations)
+
+var blueTama = new GameEngine.Sprite(200, 200, 
+32, 32, 0, 0, 'white', c, "img/bluetama.png", 
+0, 7, 0, 100, [0,1,2,3,2,0,1], animations)
+
+var playButton = new GameEngine.TamaButton(100, canvas.height-100, 25, 100, 0, 0, 'white', c, function(){
+  myTamaIdle.changeAnimation('sleep')
+})
+var feedButton = new GameEngine.TamaButton(250, canvas.height-100, 25, 100, 0, 0, 'white', c, function(){
+  myTamaIdle.changeAnimation('idle')
+})
+var statusButton = new GameEngine.TamaButton(500, canvas.height-100, 25, 100, 0, 0, 'white', c, a)
+var mateButton = new GameEngine.TamaButton(750, canvas.height-100, 25, 100, 0, 0, 'white', c, a)
+let buttons = [
+  playButton,
+  feedButton,
+  statusButton,
+  mateButton
+]
 
 spritesToRender.push(myTamaIdle)
+spritesToRender.push(myTamaIdle2)
+spritesToRender.push(blueTama)
+
+console.log(myTamaIdle.animations['sleep'].frames)
 
 var frameCount = 0;
+var now = Date.now();
+var then = now;
+var time_for_new_frame = 1000
+
+function frameController(){
+  now = Date.now()
+  console.log(now)
+  console.log(then)
+  if ((now - then) > time_for_new_frame){
+    requestAnimationFrame(step)
+    then = now;
+  }
+}
+var start = Date.now()
+console.log(start)
+var count = 0
+var currentTime = 0;
+var currentFps = 0;
 function step() {
+  currentTime = Date.now()
+  count++
+  currentFps = (count / (currentTime - start))
+
   c.clearRect(0, 0, canvas.width, canvas.height);
   c.fillStyle = 'pink'
   c.fillRect(0, 0, canvas.width, canvas.height);
-  myTamaIdle.render(); // sprite thing 350 150
 
+  spritesToRender.forEach((sprite => {
+    sprite.render()
+  }))
   buttons.forEach((button => {
     button.render()
   }))
-
-  window.requestAnimationFrame(step);
-}
-
-function init() {
-  window.requestAnimationFrame(step);
+  c.font = "24px serif";
+  c.fillText(fps + ' fps', 10, 25);
 }
 
 window.playMusic = playMusic;
@@ -87,8 +145,11 @@ function playMusic(){
 }
 
 function changeColor() {
-  const custom_color = document.querySelector('#colorpicker').value
-  document.body.style.backgroundColor = custom_color;
+  const customColor = document.querySelector('#colorpicker').value
+  document.body.style.backgroundColor = customColor;
 }
+var fps = 60
 
-init(); // call init once here instead of for every sprite
+var intervalId = window.setInterval(function(){
+  window.requestAnimationFrame(step)
+}, (1/fps) * 1000);
